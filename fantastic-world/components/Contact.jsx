c272fc1e8dd2e67e0a15274d6b12483f85c1cb12
Snadeep Tripathi <tripathisn5363@gmail.com>
2023-06-26 20:52:55 +0530

    deployed

--- fantastic-world/components/Contact.jsx
+++ fantastic-world/components/Contact.jsx
@@ -9,15 +9,15 @@ function Contact() {
         <div className="contact-info">
           <div className="contact-card">
             <img  className='emailIcon' src="https://www.freepnglogos.com/uploads/email-png/email-logo-png-master-internet-marketing-with-loretta-1.png" alt="email icon" />
-              <a href="mailto:youremail@example.com">youremail@example.com</a>
+              <a href="mailto:tripathisantosh3516@gmail.com">tripathisantosh3516@gmail.com</a>
           </div>
           <div className="contact-card">
             <img className='emailIcon' src="https://www.freepnglogos.com/uploads/logo-telepon-png/cara-mendapatkan-nomor-telpon-gratis-untuk-verifikasi-21.png" alt="phone icon" />
-            <a href="tel:+123456789">+1 234 567 89</a>
+            <a href="tel:+919336920285">9336920285</a>
           </div>
           <div className="contact-card">
             <img className='emailIcon' src="https://www.freepnglogos.com/uploads/lokasi-logo-png/lokasi-logo-google-ubicaci-imagen-gratis-28.png" alt="address icon" />
-            <p>123 Street, City, Country</p>
+            <p>Fantastic World, Lane 2, Gautam Nagar Colony, Susuwahi, Varanasi (221011)</p>
           </div>
         </div>
       </div>
